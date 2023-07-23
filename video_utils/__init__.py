9fa6ec8db0a1ed8204928cd97747e747e3683a21
Liu Ziyi <lc_lc2000@hotmail.com>
2023-07-23 23:00:23 +0800

    Remove video_utils folder.

--- video_utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .video_preprocess import create_video, merge_videos, cut_video, resize_video
-from .multithread import Worker
-
-all = [
-    'create_video',
-    'merge_videos',
-    'cut_video',
-    'resize_video'
-]
